219ca125d114efb11b8867f8f69bdaeef6daaa28
Andrius <35608576+andriusmw@users.noreply.github.com>
2022-06-05 11:11:22 +0200

    delete user a falta de comprobar login

--- repositories/users/index.js
+++ repositories/users/index.js
@@ -2,12 +2,12 @@ const insertUser = require("./insertUser");
 const selectUserByActivationCode = require("./selectUserByActivationCode");
 const deleteRegistrationCode = require("./deleteRegistrationCode");
 const selectUserByEmail = require("./selectUserByEmail");
-//const removeUser = require("./removeUser");
+const removeUser = require("./removeUser");
 
 module.exports = {
   insertUser,
   selectUserByActivationCode,
   deleteRegistrationCode,
   selectUserByEmail,
- // removeUser,
+ removeUser,
 };
